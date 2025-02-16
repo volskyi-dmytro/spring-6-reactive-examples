c8535ed438b7ec0190ebf7c8ae7be630862de738
@stpunk47 <stpunk47@gmail.com>
2025-02-16 17:23:22 +0200

    Initial commit

---
+++ src/main/java/com/stpunk47/spring_6_reactive_examples/Spring6ReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_reactive_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6ReactiveExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stpunk47/spring_6_reactive_examples/Spring6ReactiveExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stpunk47.spring_6_reactive_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
